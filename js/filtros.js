a374e1475ca04829d461c4faeb52884a5f663b01
Dan Santos <ivan.santos.v@mail.pucv.cl>
2021-01-02 22:27:03 -0300

    Se implementa la actualizacion de los filtros de busqueda

--- js/filtros.js
+++ js/filtros.js
@@ -1,32 +1,45 @@
 // Variables
-const companyId = document.querySelector('#compania');
-const userId = document.querySelector('#usuario');
-const initialDate = document.querySelector('#fecha-inicio');
-const lastDate = document.querySelector('#fecha-final');
-const intervaloMinutos = document.querySelector('#intervalo');
+const companyIdInput = document.querySelector('#compania');
+const userIdInput = document.querySelector('#usuario');
+const initialDateInput = document.querySelector('#fecha-inicio');
+const lastDateInput = document.querySelector('#fecha-final');
+const intervaloInput = document.querySelector('#intervalo');
+
+// Objeto que guarda los filtros actuales
+const filtros = {
+    companyId: -1,
+    userId: -1,
+    initialDateMs: -1,
+    lastDateMs: -1,
+    intervaloMinutos: 15
+}
 
 cargarEventsListeners();
 
 function cargarEventsListeners() {
-    companyId.addEventListener('blur', filtrarCompania);
-    userId.addEventListener('blur', filtrarUsuario);
-    initialDate.addEventListener('blur', filtrarFechas);
-    lastDate.addEventListener('blur', filtrarFechas);
-    intervaloMinutos.addEventListener('blur', filtrarIntervalo);
+    companyIdInput.addEventListener('blur', filtrarCompania);
+    userIdInput.addEventListener('blur', filtrarUsuario);
+    initialDateInput.addEventListener('change', filtrarFechas);
+    lastDateInput.addEventListener('change', filtrarFechas);
+    intervaloInput.addEventListener('blur', filtrarIntervalo);
 }
 
 function filtrarCompania(evt) {
-    console.log(evt.target);
+    filtros.companyId = (evt.target.value == '') ? -1 : parseInt(evt.target.value);
 }
 
 function filtrarUsuario(evt) {
-    console.log(evt.target);
+    filtros.userId = (evt.target.value == '') ? -1 : parseInt(evt.target.value);
 }
 
 function filtrarFechas(evt) {
-    console.log(evt.target);
+    if(evt.target.getAttribute('id') == 'fecha-inicio') {
+        filtros.initialDateMs = (evt.target.value == '') ? -1 : Date.parse(evt.target.value);
+    } else {
+        filtros.lastDateMs = (evt.target.value == '') ? -1 : Date.parse(evt.target.value);
+    }
 }
 
 function filtrarIntervalo(evt) {
-    console.log(evt.target);
+    filtros.intervaloMinutos = (evt.target.value == '') ? 15 : parseInt(evt.target.value);
 }
