b9aa0b86f4847759b819d4b5ac869028c7743954
Dan Santos <ivan.santos.v@mail.pucv.cl>
2021-01-07 13:44:34 -0300

    Se realiza una configuracion inicial de las rutas del server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const router = require('./routes/router');
 
 // Settings
 app.set('port',process.env.PORT || 3000);
@@ -9,6 +10,9 @@ app.set('port',process.env.PORT || 3000);
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+
+// Routes
+app.use('/api', router);
 
 app.listen(app.get('port'), () => {
     console.log(`Server running on port ${app.get('port')}`);
