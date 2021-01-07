bccf78c6fe707ec4a8a80a0b3538a1ef1b2ed64b
Dan Santos <ivan.santos.v@mail.pucv.cl>
2021-01-07 20:39:09 -0300

    Se implementa el metodo get de /api/logs que devuelve la fecha inicial y final de los logs

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -1,12 +1,30 @@
 const Router = require('express').Router;
 const router = Router();
+const connect = require('../database');
 
 router.get('/', (req, res) => {
     res.send('Welcome to my API!');
 })
 
 router.get('/logs', async (req, res) => {
-    res.send('Registros de logs');
+    const db = await connect();
+    const collection = db.collection('logs');
+
+    const fechasLimite = {};
+
+    // Se obtiene el primer registro, para acceder a la primera fecha
+    await collection.findOne().then(primerRegistro => {
+        fechasLimite.fechaInicial = primerRegistro.date
+    });
+
+    // Se obtiene el ultimo registro para acceder a la ultima fecha
+    await collection.find().sort({"date": -1}).limit(1)
+    .toArray()
+    .then(ultimoRegistro => {
+        fechasLimite.fechaFinal = ultimoRegistro[0].date;
+    });
+
+    res.json(fechasLimite);
 });
 
 router.post('/logs', async (req, res) => {
