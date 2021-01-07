970fcac2d5ce1bb90ed0418ca592e1a38781696b
Dan Santos <ivan.santos.v@mail.pucv.cl>
2021-01-07 14:00:10 -0300

    Se crean las rutas para los metodos get y post de /api/logs

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -2,8 +2,16 @@ const Router = require('express').Router;
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Welcome to my API!');
 })
+
+router.get('/logs', async (req, res) => {
+    res.send('Registros de logs');
+});
+
+router.post('/logs', async (req, res) => {
+    res.json(req.body)
+});
 
 
 module.exports = router;
